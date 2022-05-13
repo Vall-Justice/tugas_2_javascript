317f039f96b8cad056e7b3131566347fdc08b684
Vall-Justice <104562832+Vall-Justice@users.noreply.github.com>
2022-05-13 23:57:27 +0700

    Tugas 2 javascript

---
+++ tugas2.js
@@ -0,0 +1,25 @@
+// tugas 2 JS
+          
+// number
+let angka = 12;
+          
+          
+// string
+let nama = "Nouval";
+          
+// boolean 
+let kondisi = true;
+        
+// undefined
+let variable;
+          
+// array
+let buah = ["mangga", "apel", "nanas"];
+          
+console.log(angka);
+console.log(nama);
+console.log(kondisi);
+console.log(variable);
+console.log(buah);
+// menampilkan buah nanas
+console.log(buah[2]);
